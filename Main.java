4472c3ffee60265f4cd54dc64ee92e1c0f508171
EthanTAu <etau100@student.hbuhsd.edu>
2024-11-05 08:47:44 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main
 {
-  public static void main(String[] args)
-  {
-    // write solutions to problems here
-  }
+	public static void main(String[] args)
+	{
+		Rectangle r1 = new Rectangle(2, 4);
+		Rectangle r2 = new Rectangle(4, 2);
+    
+		boolean isRotated = (r1.getLength() == r2.getWidth()) && (r1.getWidth() == r2.getLength());
+		boolean isCongruent = isRotated || r1.equals(r2);
+		boolean isSimilar = isCongruent || ( (r1.getLength() / r2.getLength()) == (r1.getWidth() / r2.getWidth()) ) ;
+	}
 }
